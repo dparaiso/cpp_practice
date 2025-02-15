2899151cb1b596328250a4cad35d2755c31a90ca
dparaiso <danieva.paraiso@gmail.com>
2025-02-15 02:20:30 -0800

    renamed file

---
+++ functions.cpp
@@ -0,0 +1,65 @@
+#include <iostream> 
+using namespace std; 
+
+int add(int x, int y){
+    return x + y;
+}
+
+int getNumber() {
+    return 6; 
+}
+
+void test(){
+    for(int i = 0; i < 10; i++){
+        cout << i << endl; 
+    }
+}
+
+void printNTimes(int n, string text){
+    for(int i = 0; i < n; i++){
+        cout << text << endl; 
+    }
+}
+
+pair<int, int> makePair(int x, int y){
+    if(x > y){
+        return pair<int, int>(x,y); 
+    }
+    return pair<int, int>(y, x); 
+
+}
+
+// default value for z
+int doMath(int x, int y, int z=1){
+    return (x + y) * z;
+}
+
+// pass by reference
+void swap(int &x, int &y){
+    int temp = x; 
+    x=y; 
+    y=temp; 
+}
+
+// pass by pointer
+void swap(int *x, int *y){
+    int temp = *x; 
+    *x = *y; 
+    *y = temp; 
+}
+
+int main(){
+    // int res = add(2, 3); 
+    // int res2 = getNumber(); 
+    // cout << "\nThis is my result: " << res << endl; 
+    // cout << "\nThis is my result: " << getNumber() << endl; 
+    // cout << "\nThis is my result: " << getNumber() << endl; 
+    // test(); 
+
+    printNTimes(5, "tim"); 
+
+    cout << doMath(3, 4);
+    cout << makePair(2,3).first;
+
+    return 0; 
+}
