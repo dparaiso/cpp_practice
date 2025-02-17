79ba87b685ee937731586a79c0fa6c7f769c56dd
dparaiso <danieva.paraiso@gmail.com>
2025-02-16 18:02:48 -0800

    updated after first lesson

--- arrays.cpp
+++ arrays.cpp
@@ -2,7 +2,21 @@
 #include <string> 
 using namespace std; 
 
+void fillarr(int* arr){
+    for(int i = 0; i < 10; i++){
+        arr[i] = 0; 
+    }
+    return; 
+}
+
 int main(){
     int arr[5] = {1, 2, 3, 4, 5};
     string arr2[] {"tim", "is", "great"};  
+    int y[10]; 
+    fillarr(y); 
+    for(int i = 0; i < 10; i++){
+        cout << y[i] << endl;
+
+    }
 }
+// https://stackoverflow.com/questions/3473438/return-array-in-a-function
