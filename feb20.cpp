04ed41db1ce8c63212be6abe5b8ddb1af47da6d2
dparaiso <danieva.paraiso@gmail.com>
2025-02-20 11:13:40 -0800

    finished third question

--- feb20.cpp
+++ feb20.cpp
@@ -8,10 +8,42 @@ bool function(int num){
     return false;
 }
 
+
+bool revisted_function(int num){
+    if(num % 2 == 1 && num % 3 == 0){
+        return true; 
+    }
+
+    for(int i = 0; i < num; i++){
+        cout << "not valid" << endl; 
+    }
+    return false;
+}
+
+int ascii(char num){
+    return (int)num;
+}
+
 int main(){
 
+    // first question
     cout << "this is 9: " << function(9) << endl; 
     cout << "this is 4: " << function(4) << endl; 
+
+    // second question
+    char c = 'a';
+    cout << "this is the ascii code" << ascii(c) << endl; 
+
+    // int num = 5 
+    // float(num) == 5.0
+    // double num 9999999
+    // (int)num = 999
+
+    // third question
+    cout << "this is 9: " << revisted_function(9) << endl; 
+    cout << "this is 4: " << revisted_function(4) << endl;    
+
+
     return 0; 
 }
 
@@ -19,7 +51,8 @@ int main(){
 
 // write a function that takes in a character and returns the ascii code (typecasting)
  
-// write a file io function 
+// go back to the first function and edit it so that if a number is not odd or divisable by three it will print out "not valid" n (number) amount of times
+
 /*
     expressions and interactivity 
     decision structures 
