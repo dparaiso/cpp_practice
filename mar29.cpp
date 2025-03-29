76120f51ccea02d5a340adc1ce265be3e712d790
dparaiso <danieva.paraiso@gmail.com>
2025-03-29 16:16:42 -0700

    updating mar 29

---
+++ mar29.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std; 
+
+int main(){
+
+    string test;
+
+    // test.isDigit(); 
+
+    // char test = ['h', 'e', 'l', 'l', '0']; 
+    string test2 = "6hello"; 
+    cout << endl << isdigit(test2[0]);
+    cout << endl << test2.size(); 
+
+    std::cout << "Original Number: " << input << " " <<  endl; 
+    
+    return 0; 
+}
+
+
+
+
+12345
+[1, 2, 3, 4, 5]
+
+
+1 * 10 = 10 + 2 = 12 * 10 + 3
+2
+3
+4
+5
+
+
+7 
+if (input.size() == 1){
+    add = (int)input[position] -48;
+}
+7
