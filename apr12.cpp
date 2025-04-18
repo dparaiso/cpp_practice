e7860d3a4cccecdaddc7e582eb80ecba69f37ac9
dparaiso <danieva.paraiso@gmail.com>
2025-04-17 17:17:41 -0700

    april 17th session

--- apr12.cpp
+++ apr12.cpp
@@ -9,20 +9,10 @@ int sum(int k) {
     }
   }
 
+  // fibbonacci sequence 
 int main(){
-    // try {
-    //     int age = 15;
-    //     if (age >= 18) {
-    //       cout << "Access granted - you are old enough.";
-    //     } else {
-    //       throw 505;
-    //     }
-    //   }
-    //   catch (int myNum) {
-    //     cout << "Access denied - You must be at least 18 years old.\n";
-    //     cout << "Error number: " << myNum;
-    //   }
-
+  
     int result = sum(10);
     cout <<endl <<  result << endl;
+ 
 }
